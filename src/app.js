ceb9afdab00711d41b0c2ca740a0f8e69818cfd8
Mili83666 <milagrocaba@gmail.com>
2025-06-25 19:21:46 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,45 @@
 //write your code here
-console.log("Hello Rigo from the console!");
+
+    const opcions = ["piedra", "papel", "tijera", "lagarto", "spock"];
+
+    const player = prompt("Elige piedra, papel, tijera, lagarto, spock").toLowerCase();
+
+    const numeroAleatorio = () => {
+        const index = Math.floor(Math.random() * opcions.length)
+        return opcions [index];
+    }
+
+
+    const opcionComputer = numeroAleatorio()
+
+
+
+    // Reglas del juego
+    const aJugar = (player, computer) => {
+
+
+        if (player === computer) {
+            console.log("Empate!!!")
+        }
+        else if (
+            (player === "piedra" && (computer === "tijera" || computer === "lagarto")) ||
+            (player === "papel" && (computer === "piedra" || computer === "spock")) ||
+            (player === "tijera" && (computer === "papel" || computer === "lagarto")) ||
+            (player === "lagarto" && (computer === "spock" || computer === "papel")) ||
+            (player === "spock" && (computer === "tijera" || computer === "piedra"))
+        ) {
+            console.log("¡Ganaste!");
+        } else {
+            console.log("¡Perdiste!");
+        }
+    }
+
+aJugar(player, opcionComputer)
+
+
+
+
+
+
+
 
